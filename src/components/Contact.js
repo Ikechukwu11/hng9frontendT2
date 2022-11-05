cd688c1da225ba46d29c26a124d9e52daf547f0c
Ikechukwu.kev <kevinuzoma@gmail.com>
2022-11-05 08:56:25 +0100

    feat: added bootstrap dependency

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,86 +10,32 @@ export default function Contact(){
         <p>
          Hi there, contact me to ask me about anything you have in mind.
         </p>
+
         
+        <form >
+        <div className="mb-3 ">
+          <label className="form-label" htmlFor="name">
+            Name
+          </label>
+          <input className="form-control" type="text" id="name" required />
+        </div>
+        <div className="mb-3">
+          <label className="form-label" htmlFor="email">
+            Email
+          </label>
+          <input className="form-control" type="email" id="email" required />
+        </div>
+        <div className="mb-3">
+          <label className="form-label" htmlFor="message">
+            Message
+          </label>
+          <textarea className="form-control" id="message" required />
+        </div>
+        <button className="btn btn-danger" type="submit">
+          
+        </button>
+      </form>
         
-        <div className='App-link-div'>
-        <a id='twitter'
-          className="App-link"
-          href="https://twitter.com/ikechukwukev"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         @ikechukwukev
-        </a>
-        </div>
-
-        <div className='App-link-div'>
-        <a id='btn__zuri'
-          className="App-link"
-          href="https://training.zuri.team/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Zuri Team
-        </a>
-        </div>
-
-        <div className='App-link-div'>
-        <a id='books'
-          className="App-link"
-          href="https://books.zuri.team/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Zuri Books
-        </a>
-        </div>
-
-        <div className='App-link-div'>
-        <a id='book__python'
-          className="App-link"
-          href="https://books.zuri.team/python-for-beginners?ref_id=%3Ckekesmovic%3E"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Python Books
-        </a>
-        </div>
-
-        <div className='App-link-div'>
-        <a id='pitch'
-          className="App-link"
-          href="https://background.zuri.team/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Background Check for Coders
-        </a>
-        </div>
-        
-        <div className='App-link-div'>
-        <a id='book__design'
-          className="App-link"
-          href="https://books.zuri.team/design-rules"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Design Books
-        </a>
-        </div>
-
-        <div className='App-link-div'>
-        <Link to="contact">
-        <a id='contact'
-          className="App-link"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Contact Me
-        </a>
-        </Link>
-        </div>
-
         <div className='App-link-div'>
         <a
           className="App-link"
