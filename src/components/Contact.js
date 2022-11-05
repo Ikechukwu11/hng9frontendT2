3e0ab94e2c734a64687aa1ceb4179fea3c9b0edd
Ikechukwu.kev <kevinuzoma@gmail.com>
2022-11-05 09:22:56 +0100

    feat : Added bootstrap dependency

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,48 +12,38 @@ export default function Contact(){
         </p>
 
         
-        <form >
-        <div className="mb-3 ">
-          <label className="form-label" htmlFor="name">
-            Name
-          </label>
-          <input className="form-control" type="text" id="name" required />
-        </div>
-        <div className="mb-3">
-          <label className="form-label" htmlFor="email">
-            Email
-          </label>
-          <input className="form-control" type="email" id="email" required />
-        </div>
-        <div className="mb-3">
-          <label className="form-label" htmlFor="message">
-            Message
-          </label>
-          <textarea className="form-control" id="message" required />
-        </div>
-        <button className="btn btn-danger" type="submit">
-          
-        </button>
-      </form>
+        <form className="container mt-5">
+          <div className="row">
+            <div className="form-group col-md-6">
+              <label htmlFor="fname">First name</label>
+              <input type="text" placeholder='Enter your first name' className="form-control" id="fname" />
+            </div>
+            <div className="form-group col-md-6">
+              <label htmlFor="lname">Last name</label>
+              <input type="text" placeholder='Enter your last name' className="form-control" id="lname" />
+            </div>
+          </div>
+          <div className="form-group">
+            <label htmlFor="Address">Email</label>
+            <input type="text" className="form-control" id="mail" placeholder="yourname@email.com" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="message">Message</label>
+            <textarea className="form-control" rows ="3" id='message' placeholder="Send me a message and I'll reply you as soon as possible...">
+
+            </textarea>
+          </div>
         
-        <div className='App-link-div'>
-        <a
-          className="App-link"
-          href="https://hng9.slack.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         <img src={'./img/slack.svg'} id="profile_img" className="social_svg" alt="slack_svg" />
-        </a>
-        <a
-          className="App-link"
-          href="https://github.com/Ikechukwu11"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         <img src={'./img/github.svg'} id="profile_img" className="social_svg" alt="github_svg" />
-        </a>
-        </div>
+          <div className="form-group">
+            <div className="form-check">
+              <input className="form-check-input" type="checkbox" id="gridCheck" />
+              <label className="form-check-label" htmlFor="gridCheck">
+                Check me out
+              </label>
+            </div>
+          </div>
+          <button type="submit" className="btn btn-primary">Sign in</button>
+        </form>
         
       </header>
       <footer className='App-footer'>
